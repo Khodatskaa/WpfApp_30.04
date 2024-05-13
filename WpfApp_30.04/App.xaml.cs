cd4eb8cd8f8be1d70cef0082d77114f2115dec65
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-05-13 11:57:00 +0300

    Add project files.

---
+++ WpfApp_30.04/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_30._04
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
